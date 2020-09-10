25ea07e2a4b7983ecc7e123bf0f1ff2492d59a5b
Gazmir Mazari <mazarig@mail.gvsu.edu>
2020-09-10 16:34:34 -0400

    FIx the raw input

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 import json
-import difflib 
-
+from difflib import get_close_matches
+    
 data = json.load(open("data.json"))
-
-def meaning(word):
-    if word in data:
-        return data[word]
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
-        return "This word doesn't exist. Please double check it!"
+        return "The word doesn't exist. Please double check it."
+word = raw_input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
 
 
-word = input ("Enter a word: ").lower()
-
-
-print(meaning(word))    
 
